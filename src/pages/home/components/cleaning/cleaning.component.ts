2817d5ea9825b66529bd8be9d8010d33e4aadda4
Marina Fernandez <marinaffc@gmail.com>
2018-05-28 00:10:51 +0200

    Trabajo de navegación sencillo: punto inicio

---
+++ src/pages/home/components/cleaning/cleaning.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'app-cleaning',
+  templateUrl: 'cleaning.component.html'
+})
+export class CleaningComponent {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CleaningComponent } from './components/cleaning/cleaning.component';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  pages: any[] = [];
   constructor(public navCtrl: NavController) {
+    this.pages = [ {
+      title: 'Limpieza',
+      component: CleaningComponent
+    },
+    {
+      title: 'Cocina',
+      component: CleaningComponent
+    },
+    {
+      title: 'Series',
+      component: CleaningComponent
+    },
+    
+    {
+      title: 'Cine',
+      component: CleaningComponent
+    },
+    {
+      title: 'Música',
+      component: CleaningComponent
+    },
+    {
+      title: 'Eventos',
+      component: CleaningComponent
+    },
+  ];
+  }
 
+  openPage(event){
+    this.navCtrl.push(event.component, {
+      item: 'id'
+    });
   }
 
 }
